e78f1045a5b5c8eeafa58e7361c238451b575ef1
HelloCodeman <955khm@gmail.com>
2024-01-22 11:22:48 +0800

    完成最新消息前台顯示功能及跑馬燈

---
+++ front/news.php
@@ -0,0 +1,13 @@
+<h2 class="ct">最新消息</h2>
+<!-- table.all>(tr.tt>td)+(tr.pp*2>td) -->
+<table class="all ct">
+  <tr class="tt ct">
+    <td>標題</td>
+  </tr>
+  <tr class="pp">
+    <td>情人節特惠活動</td>
+  </tr>
+  <tr class="pp">
+    <td>年終特賣會開跑</td>
+  </tr>
+</table>
