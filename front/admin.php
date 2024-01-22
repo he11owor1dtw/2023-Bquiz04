768321931bfc86c69b4c64e3f900e3e7d8d1628a
HelloCodeman <955khm@gmail.com>
2024-01-22 13:59:58 +0800

    登入頁增加驗證碼功能,同時複製一份給管理員登入使用

---
+++ front/admin.php
@@ -0,0 +1,26 @@
+<h2 style="text-align: center;">管理員登入</h2>
+
+  <!-- table.all>tr*3>td.tt.ct+td.pp>input:text -->
+  <table class="all">
+    <tr>
+      <td class="tt ct">帳號</td>
+      <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+      <td class="tt ct">密碼</td>
+      <td class="pp"><input type="text" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+      <td class="tt ct">驗證碼</td>
+      <td class="pp">
+        <?php
+        $a = rand(10, 99);
+        $b = rand(10, 99);
+        $_SESSION['ans'] = $a + $b;
+        echo $a . "+" . $b . "=";
+        ?>
+        <input type="text" name="chk" id="chk">
+      </td>
+    </tr>
+  </table>
+  <div class="ct"><button>確認</button></div>
--- front/login.php
+++ front/login.php
@@ -1,7 +1,4 @@
-<h2>第一次購買</h2>
-<img src="./icon/0413.jpg" onclick="location.href='?do=reg'">
-
-<h2>會員登入</h2>
+<h2 style="text-align: center;">會員登入</h2>
 <!-- table.all>tr*3>td.tt.ct+td.pp>input:text -->
 <table class="all">
   <tr>
@@ -14,7 +11,14 @@
   </tr>
   <tr>
     <td class="tt ct">驗證碼</td>
-    <td class="pp"><input type="text" name="chk" id="chk"></td>
+    <td class="pp">
+      <?php
+      $a=rand(10,99);
+      $b=rand(10,99);
+      $_SESSION['ans']=$a+$b;
+      echo $a . "+" . $b . "=";
+      ?>
+      <input type="text" name="chk" id="chk"></td>
   </tr>
 </table>
 <div class="ct"><button>確認</button></div>
