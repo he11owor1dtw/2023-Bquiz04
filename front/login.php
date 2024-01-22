49a340b4f09248f37ba2da09519f120757119572
HelloCodeman <955khm@gmail.com>
2024-01-22 14:15:09 +0800

    建立會員註冊頁面

--- front/login.php
+++ front/login.php
@@ -1,3 +1,7 @@
+<h2 style="text-align: center;">第一次購買
+<br>
+  <img src="./icon/0413.jpg" onclick="location.href='?do=reg'">
+</h2>
 <h2 style="text-align: center;">會員登入</h2>
 <!-- table.all>tr*3>td.tt.ct+td.pp>input:text -->
 <table class="all">
@@ -13,12 +17,13 @@
     <td class="tt ct">驗證碼</td>
     <td class="pp">
       <?php
-      $a=rand(10,99);
-      $b=rand(10,99);
-      $_SESSION['ans']=$a+$b;
+      $a = rand(10, 99);
+      $b = rand(10, 99);
+      $_SESSION['ans'] = $a + $b;
       echo $a . "+" . $b . "=";
       ?>
-      <input type="text" name="chk" id="chk"></td>
+      <input type="text" name="chk" id="chk">
+    </td>
   </tr>
 </table>
 <div class="ct"><button>確認</button></div>
