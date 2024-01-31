9856f4a895a783dcb87c35616ba8644dfd1be608
HelloCodeman <955khm@gmail.com>
2024-01-31 14:40:08 +0800

    修正typo並把script區塊往下放

--- api/get_types.php
+++ api/get_types.php
@@ -5,5 +5,5 @@ include_once "db.php";
 $types = $Type->all($_GET);
 
 foreach ($types as $type) {
-  echo "<option value='{type['id']}'>{$type['name']}</option>";
+  echo "<option value='{$type['id']}'>{$type['name']}</option>";
 }
